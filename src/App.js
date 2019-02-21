62be7e8c74b7935894a2dd233895674141d9b6f6
Ivan Gonzalez <ivandario1998@gmail.com>
2019-02-21 02:26:11 -0500

    Basic example of using Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      useremail: "",
+      userpass: ""
+    }
+  }
+
+  login = () => {
+    const email = this.state.useremail;
+    const password = this.state.userpass;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode + ": " + errorMessage);
+    }).then(function (success) {
+      console.log(success);
+      console.log("Login success");      
+    });
+  }
+
+  get = () => {
+    const user = firebase.auth().currentUser;
+    console.log(user);
+  }
+
+  logout = () => {
+    firebase.auth().signOut().then(function (success) {
+      console.log("Logout success");
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h4>Login</h4>
+          <form>
+            Email: <input type="text" name="useremail" onChange={event => this.setState({ useremail: event.target.value })} />
+            <br />
+            Password: <input type="password" name="userpass" autoComplete="userpass" onChange={event => this.setState({ userpass: event.target.value })} />
+          </form>
+          <br />
+          <button onClick={this.login}>Login</button>
+          <br />
+          <button onClick={this.get}>Get user</button>
+          <br />
+          <button onClick={this.logout}>Logout</button>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "Search in your account",
+    authDomain: "Search in your account",
+    databaseURL: "Search in your account",
+    projectId: "Search in your account",
+    storageBucket: "",
+    messagingSenderId: "Search in your account"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
